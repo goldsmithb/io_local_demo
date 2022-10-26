c26833c0618b3868de3dc0ef9e5a18ff5fae5bfa
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-10-26 11:25:54 -0400

    upvote downvote working

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -5,16 +5,20 @@ import Post from './Post'
 export default function NewsFeed() {
   const [isFullscreen, setIsFullscreen] = useState(false);
   const [examplePosts, setExamplePosts] = useState([{
+    id: 1,
     title: "Example",
     author: "Bob S.",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec velit non lorem ultrices aliquet non eu risus. Vivamus euismod sapien sit amet eros gravida pellentesque vitae at sem. Nam at aliquam sapien. Aliquam et tortor sit amet urna laoreet dignissim. Mauris imperdiet blandit varius. Sed in varius quam. Morbi volutpat porta dolor, et ornare augue fringilla sit amet. Praesent non urna orci. Duis orci nisi, congue sed est non, fringilla volutpat elit. Curabitur lacus ipsum, dictum ac dolor non, viverra dignissim nisi.",
-    type: "social"
+    type: "social",
+    score: 0
   },
   {
+    id: 2,
     title: "Example 2",
     author: "Karen S.",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec velit non lorem ultrices aliquet non eu risus. Vivamus euismod sapien sit amet eros gravida pellentesque vitae at sem. Nam at aliquam sapien. Aliquam et tortor sit amet urna laoreet dignissim. Mauris imperdiet blandit varius. Sed in varius quam. Morbi volutpat porta dolor, et ornare augue fringilla sit amet. Praesent non urna orci. Duis orci nisi, congue sed est non, fringilla volutpat elit. Curabitur lacus ipsum, dictum ac dolor non, viverra dignissim nisi.",
-    type: "market"
+    type: "market",
+    score: 100
   }]);
 
   function clickFullscreen() { 
@@ -24,33 +28,10 @@ export default function NewsFeed() {
   return (
     <div className="news-feed">
       {examplePosts.map((post) => (
-        <div className={"post wireframe " + (isFullscreen ? "fullscreen" : "")}>
-          <div className="post__header">
-            <div className="post__header-icon wireframe">Icon</div>
-            <span>{post.title}</span>
-            <input onClick={clickFullscreen} type="image" id="image" alt="Full screen" src={fullscreenButtonImg} />
-          </div>
-
-          <div className="post__content">{post.content}</div>
-
-          <div className="post__footer">
-            <span>{post.author}</span>
-            <div className="post__controls">
-              <div>upvote</div>
-              <div>downvote</div>
-              <div>comments</div>
-            </div>
-          </div>
-        </div>
+        <Post post={post} key={Math.floor(Math.random()*1000)} isFullscreen={isFullscreen} clickFullscreen={clickFullscreen}/>
       ))
       // we still need some conditional things for different post types
       }
     </div>
-    
-    // <div className={"news-feed " + (isFullscreen ? "fullscreen-backdrop" : "")}>
-    //   <Post isFullscreen={isFullscreen} clickFullscreen={clickFullscreen}/>
-    //   <Post />
-    //   <Post />
-    // </div>
   )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -8,31 +8,41 @@ Post content:
     title: "Example",
     author: "Bob S.",
     content: ""...",
-    type: "social" | "market" | "request"
+    type: "social" | "market" | "request",
+    score: int
   }
 */
 
 import { useState } from 'react'
 import './Post.css'
 import fullscreenButtonImg from '../media/fullscreen_button.png';
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 
-export default function Post({ isFullscreen, clickFullscreen }) {
+export default function Post({ post, key, isFullscreen, clickFullscreen }) {
+  const [id, setId] = useState(post.id);
+  const [title, setTitle] = useState(post.title);
+  const [author, setAuthor] = useState(post.author);
+  const [content, setContent] = useState(post.content);
+  const [type, setType] = useState(post.type);
+  const [score, setScore] = useState(post.score);
+
   return (
-    <div className={"post wireframe " + (isFullscreen ? "fullscreen" : "")}>
+    <div key={id} className={"post wireframe " + (isFullscreen ? "fullscreen" : "")}>
       <div className="post__header">
         <div className="post__header-icon wireframe">Icon</div>
-        <span>Title</span>
+        <span>{title}</span>
         <input onClick={clickFullscreen} type="image" id="image" alt="Full screen" src={fullscreenButtonImg} />
-        </div>
+      </div>
 
-      <div className="post__content">Main content</div>
-      
+      <div className="post__content">{content}</div>
+
       <div className="post__footer">
-        <span>Author</span>
+        <span>{author}</span>
         <div className="post__controls">
-          <div>upvote</div>
-          <div>downvote</div>
-          <div>comments</div>
+          <span className="score">{score}</span>
+          <span onClick={() => setScore(score + 1)}>upvote</span>
+          <span onClick={() => setScore(score - 1)}>downvote</span>
+          <span>comments</span>
         </div>
       </div>
     </div>
