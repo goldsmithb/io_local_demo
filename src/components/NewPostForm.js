9e6a89d7b454faf100e62a1a42fc0001cbdf4608
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-10-26 15:46:49 -0400

    create controlled input for newPostForm and handleSubmit fn

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,16 +1,44 @@
 import { useState } from 'react';
 
-export default function NewPostForm() {
+export default function NewPostForm({ addNewPost }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const resetForm = () => {
+    setTitle("");
+    setContent("");
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = {
+      title: title,
+      author: "current user",
+      content: content,
+      type: "social",
+      score: 0
+    }
+    addNewPost(newPost);
+    resetForm();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         <span>Title:</span>
-        <input type="text" />
+        <input 
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title} />
       </label>
       <label>
         <span>Content:</span>
-        <input type="text" />
+        <input 
+          type="text" 
+          onChange={(e) => setContent(e.target.value)}
+          value={content} />
       </label>
+      <button>Submit</button>
     </form>
   )
 }
