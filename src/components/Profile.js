df84aab2f9a2d1793926542e87dd741d1173af18
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-10-29 12:30:55 -0400

    setup profile page content

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,18 @@
+import styles from './Profile.module.css'
+
 export default function Profile({ changePage }) {
   return (
-    <div>Profile</div>
+    <div className={styles.profile}>
+      <div className={styles['top-bar']}>
+        <img alt="Profile picture"/>
+        <div className={styles.bio}>
+          <h2>John Doe</h2>
+          <p>Read all about me!</p>
+        </div>
+      </div>
+
+      <p>Post history</p>
+    </div>
+
   )
 }
