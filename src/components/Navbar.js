3d54e0f74ae67464809cde9f5eb6bf6cc06d4043
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-10-31 14:29:22 -0400

    delete old style obj

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,5 @@
 import styles from './Navbar.module.css';
 
-const gstyles = {
-  header: {
-    backgroundColor: "#b8b8b8",
-    color: 'white',
-    width: '100%',
-    margin: '0',
-    padding: '0 15%',
-
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  header_ul: {
-    listStyle: 'none',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    gap: '30px'
-  },
-  header_logo: {
-    height: '35px',
-    width: '35px',
-    display: 'inline' // doesnt give desired overlay effect - rm?
-  }
-}
-
 export default function Navbar({ changePage }){
   console.log("rendering navbar - Navbar");
   return (
